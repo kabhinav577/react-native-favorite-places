9eb339eb17ea3a9184014044b6052c20390eba2d
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-03-09 20:04:26 +0530

    feat: setting up components and screens

---
+++ components/Places/PlaceItem.js
@@ -0,0 +1,17 @@
+import { Image, Pressable, View, StyleSheet, Text } from 'react-native';
+
+const PlaceItem = ({ place, onSelect }) => {
+  return (
+    <Pressable>
+      <Image source={{ uri: place.imageUri }} />
+      <View>
+        <Text>{place.title}</Text>
+        <Text>{place.address}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+export default PlaceItem;
+
+const styles = StyleSheet.create({});
